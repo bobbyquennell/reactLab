d9ba616c42e335a47960e5fc2d46b49ae1c81d68
bobbyquennell <bobbyquennell@gmail.com>
2017-05-12 13:58:14 +1000

    1_firstReactApp_create buttons, add increments to result

--- react_lab/src/index.js
+++ react_lab/src/index.js
@@ -1,9 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+class Button extends React.Component {
+
+    incrementHandler = ()=>{
+    	this.props.onClickHandler(this.props.incrementStep);
+    }
+
+	render = (props) => {
+		return (
+			<button onClick={this.incrementHandler}>+{this.props.incrementStep}</button>
+			)
+	}
+}
+
+
+const Result = (props)=>{
+	return (
+			<div>{props.counter}</div>
+		);
+}
+class ClrRst extends React.Component{
+
+	render(props){
+		return(
+			<button onClick={this.props.onClickHandler}>Clear</button>
+			)
+	}
+
+}
+
+class App extends React.Component{
+		constructor(props){
+		super(props);
+		this.state = {counter:0};
+        this.handleClick = this.handleClick.bind(this);
+	}
+    handleClick = (value)=>{
+      this.setState((prevState)=>({
+      	counter:prevState.counter + value
+      }
+      ));
+	}
+	clrRstClick = ()=>{
+      this.setState((prevState)=>({
+      	counter:0
+      }
+      ));
+	}
+	render(){
+		return (
+		<div>
+			<Button onClickHandler={this.handleClick} incrementStep={1}/>
+			<Button onClickHandler={this.handleClick} incrementStep={5}/>
+			<Button onClickHandler={this.handleClick} incrementStep={10}/>
+			<Button onClickHandler={this.handleClick} incrementStep={20}/>
+			<Button onClickHandler={this.handleClick} incrementStep={100}/>
+			<ClrRst onClickHandler={this.clrRstClick}/>
+			<Result counter={this.state.counter}/>
+		</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
