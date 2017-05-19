ee375c9e1f7834b65c5c553ce0f59ae37a21aa53
bobbyquennell <bobbyquennell@gmail.com>
2017-05-19 16:18:17 +1000

    user interface design

--- react_lab/src/e3.js
+++ react_lab/src/e3.js
@@ -3,46 +3,52 @@ import './App.css';
 /*this is an App for React Js Getting Started by Samer Buna, Chapter 4: Building the Game interface*/
 
 
-const Star=()=>{
+const Star=(props)=>{
+   const starNumber = Math.floor(Math.random()*9) + 1;
+   var stars = [];
+   //react encourages using array methods like: map, filter, reduce instead of loops
+   for (let i = 0; i < starNumber; i++) {
+   	   stars.push(<i key={i} className="fa fa-star"></i>);
+   }
 	return(
 		 <div className="col-5">
-		 <i className="fa fa-star"></i>
-		 <i className="fa fa-star"></i>
-		 <i className="fa fa-star"></i>
-		 <i className="fa fa-star"></i>
+			{stars}
 		 </div>
 
 		)
 }
 const Answer=()=>{
 	return(
-		 <div className="col-2">
-		 ...
+		 <div className="col-5">
+			<span>5</span>
+			<span>6</span>
 		 </div>
 
 		)
 }
 const Button=()=>{
 	return(
-		 <div className="col-5">
+		 <div className="col-2">
 		 <button className="btn btn-default btn-lg">=</button>
 		 </div>
 
 		)
 }
 const Numbers=()=>{
+	const numbers = [1,2,3,4,5,6,7,8,9];
 	return(
-		<div className="text-center">
-			<span className="notselected">1</span>
+		<div className=" card text-center">
+{/*			<span>1</span>
 			<span className="selected">2</span>
 			<span className="used">3</span>
-			<span>4</span>
+			<span>4</span>*/}
+			{numbers.map((number, i)=>{return (<span key={i}>{number}</span>);})}
 		</div>
 		)
 }
 const Game = ()=>{
 	return (
-		<div>
+		<div className="container">
 			<h2>Play Nine</h2>
 			<hr/>
 			<div className="row">
