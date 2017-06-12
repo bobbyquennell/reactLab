d81a3b9f395260a4d3894e4e984c36cce177b12a
bobbyquennell <bobbyquennell@gmail.com>
2017-06-12 22:32:41 +1000

    Add Thunk to store and Create Load Courses Thunk

--- react_lab/src/actions/courseActions.js
+++ react_lab/src/actions/courseActions.js
@@ -1,6 +1,18 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 export function createCourse(course) {
   //debugger;
   console.log("Redux Flow step 2: Action Creator: createCouse " + course);
   return {type:types.CREATE_COURSE, course};//type property is required for Redux
+}
+export function loadCoursesSuccess(courses){
+  return{type: LOAD_COURSES_SUCCESS, courses}
+}
+
+export function loadCourses(){
+  return function(dispatch){
+     return courseApi.getAllCourses().then(courses =>{
+        dispatch(loadCoursesSuccess(courses));
+     }).catch(error =>{ throw(error);});
+  }
 }
