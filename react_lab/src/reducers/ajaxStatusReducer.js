0d0fdb615ff65d129c9c43a919317d7e7b4592bc
bobbyquennell <bobbyquennell@gmail.com>
2017-06-16 15:15:15 +1000

    Create ajaxStatusReducer

---
+++ react_lab/src/reducers/ajaxStatusReducer.js
@@ -0,0 +1,27 @@
+import * as types from '../actions/actionTypes'
+import initialState from './initialState';
+
+function actionTypeEndsInSuccess(type){
+    return type.substring(type.length -8) == '_SUCCESS';
+}
+
+export default function ajaxStatusReducer(state=initialState.numAjaxCallsInProgress, action){
+     if(action.type == types.BEGIN_AJAX_CALL){
+       return state + 1;
+
+     }
+     else if (actionTypeEndsInSuccess(action.type))
+      //our thunks ultimately dispatch a success action when they complete. That means that I can use the success suffix '_SUCCESS' as a
+       //signal that the action is complelted.this will help us avoid manually dispatching a separate endAjaxCall action every time
+       // an Ajax  call is completed
+     {
+       return state -1;
+       //note: we will handling the same action in multiple reducers
+       //any action types that ends in SUCCESS will now be handled here as well as another reducer
+       //and this is a powerfull method: each reducer is simply a slice of state, so a given action may impact multiple reducers.
+       
+     }
+     else{
+       return state;
+     }
+  }
--- react_lab/src/reducers/index.js
+++ react_lab/src/reducers/index.js
@@ -1,9 +1,13 @@
 import {combineReducers} from 'redux';
 import courseReducer from './courseReducer';
 import authorReducer from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';//ajaxCallsInProgress is the alias for ajaxStatusReducer
+//see: https://stackoverflow.com/questions/39282253/how-can-i-alias-a-default-import-in-javascript
+
 const rootReducer = combineReducers({
   courseReducer,//es6 shorthand property name:equals to courses:courses
-  authorReducer
+  authorReducer,
+  ajaxCallsInProgress
 });
 
 export default rootReducer;
