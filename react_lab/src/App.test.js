9ea215b3fb5ad8857d4a88298287de7a853460be
bobbyquennell <bobbyquennell@gmail.com>
2017-06-19 17:00:41 +1000

    testing setup, failed

--- react_lab/src/App.test.js
+++ react_lab/src/App.test.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/App';
+// import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+//
+// import HomePage from './home/HomePage';
+// import AboutPage from './about/AboutPage';
+// import Header from './common/Header';
+// import CoursesPage from './courses/CoursesPage';
+// import ManageCoursePage from './courses/ManageCoursePage';
+// import {connect} from 'react-redux';
+//
+// it('renders without crashing', () => {
+//   ReactDOM.render(<App />, div);
+// });
---
+++ react_lab/src/components/courses/CourseForm.test.js
@@ -0,0 +1,48 @@
+import expect from 'expect';
+import React from 'react';
+//import TestUtils from 'react-addons-test-utils'; //TestUtils have been moved to react-dom/test-utils
+//https://www.npmjs.com/package/react-addons-test-utils
+import ShallowRenderer from 'react-test-renderer/shallow';//Shallow renderer has been moved to react-test-renderer/shallow
+//import ReactTestRenderer from 'react-test-renderer';
+import CourseForm from './CourseForm';
+
+function setup(saving){
+  let props ={
+    course:{}, saving:saving, errors: {},
+    onSave:()=>{},
+    onChange:()=>{}
+  };
+  //let renderer = TestUtils.createRender();
+  let renderer = new ShallowRenderer();
+  debugger;
+  renderer.render(<CourseForm {...props}/>);
+  let output = renderer.getRenderOutput();
+
+  return {
+    props,
+    output,
+    renderer
+  };
+}
+
+describe('CourseForm via React Test Utils', ()=>{
+  it('renders from and h1', ()=>{
+     const {output} = setup(false);
+     expect(output.type).toBe('form');
+     let [h1] = output.props.children;
+     expect(h1.type).toBe(h1);
+
+  });
+
+  it('save button is labeled "Save" when not saving', ()=>{
+    const{output} = setup(false);
+    const submitButton = output.props.children[5];
+    expect(submitButton.props.value).toBe('Save');
+  });
+
+  it('save button is labeled "Saving..." when saving', ()=>{
+    const { output } = setup(true);
+    const submitButton = output.props.children[5];
+    expect(submitButton.props.value).toBe('Saving...');
+  })
+});
