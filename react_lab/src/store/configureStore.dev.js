7acbbfdfee2e7d9dd3246cc265412db584f2806d
bobbyquennell <bobbyquennell@gmail.com>
2017-06-29 11:37:36 +1000

    set up redux dev tool extension for chrome

--- react_lab/src/store/configureStore.dev.js
+++ react_lab/src/store/configureStore.dev.js
@@ -3,9 +3,10 @@ import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 export default function configureStore(initialState){
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     return createStore(
-        rootReducer,
+        rootReducer,/* preloadedState, */
         initialState,
-        applyMiddleware(thunk, reduxImmutableStateInvariant())
+        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
     );
 }
