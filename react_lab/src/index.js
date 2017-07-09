cbac0841eef94bfc72d6f65cdbfbc3e8a4d934f7
bobbyquennell <bobbyquennell@gmail.com>
2017-07-09 17:25:41 +1000

    re-structure project folder

--- react_lab/src/index.js
+++ react_lab/src/index.js
@@ -2,11 +2,11 @@
 //import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';//'./e1.js';//'./components/App';//swtich between different apps by editing the path here
-import {loadCourses} from './actions/courseActions';
-import {loadAuthors} from './actions/authorActions';
+import App from './reactGetStartedExamples/e3';//'./reactGetStartedExamples/e1';//'./reduxExample/components/App';//swtich between different apps by editing the path here
+import {loadCourses} from './reduxExample/actions/courseActions';
+import {loadAuthors} from './reduxExample/actions/authorActions';
 import './index.css';
-import configureStore from './store/configStore';
+import configureStore from './reduxExample/store/configStore';
 import {Provider} from 'react-redux';
 import '../node_modules/toastr/build/toastr.min.css';
 
